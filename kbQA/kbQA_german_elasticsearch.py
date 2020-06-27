d4940374b2c370467d78d272ce20420bc0cfbcfb
Jonas <jonas_liebermann@web.de>
2020-06-27 12:48:44 +0200

    format

--- kbQA/kbQA_german_elasticsearch.py
+++ kbQA/kbQA_german_elasticsearch.py
@@ -16,17 +16,19 @@ from haystack.retriever.elasticsearch import ElasticsearchRetriever
 if __name__ == '__main__':
     # Start new server or connect to a running one. true and false respectively
     LAUNCH_ELASTICSEARCH = False
-    # Determines whether the Elasticsearch Server has to be populated with data or not
+    # Determines whether the Elasticsearch Server has to be populated with data
+    # or not
     POPULATE_DOCUMENT_STORE = False
-
     # Start an Elasticsearch server
     if LAUNCH_ELASTICSEARCH:
         logging.info("Starting Elasticsearch ...")
         status = subprocess.run(
-            ['docker run -d -p 9200:9200 -e "discovery.type=single-node" elasticsearch:7.6.2'], shell=True
+            ['docker run -d -p 9200:9200 -e "discovery.type=single-node" elasticsearch:7.6.2'],
+            shell=True
         )
         if status.returncode:
-            raise Exception("Failed to launch Elasticsearch. If you want to connect to an existing Elasticsearch instance"
+            raise Exception("Failed to launch Elasticsearch. If you want to "
+                            "connect to an existing Elasticsearch instance"
                             "then set LAUNCH_ELASTICSEARCH in the script to False.")
         time.sleep(15)
 
@@ -40,7 +42,8 @@ if __name__ == '__main__':
     if POPULATE_DOCUMENT_STORE:
         # set path to directory containing the text files
         doc_dir = os.getcwd() + "\\kbQA\\data\\tesla"
-        # convert files to dicts containing documents that can be indexed to our datastore
+        # convert files to dicts containing documents that can be indexed to our
+        # datastore
         dicts = convert_files_to_dicts(
             dir_path=doc_dir, clean_func=clean_wiki_text, split_paragraphs=True)
 
@@ -48,33 +51,24 @@ if __name__ == '__main__':
         document_store.write_documents(dicts)
 
     # ## Initalize Retriever, Reader,  & Finder
-    #
     # ### Retriever
-    #
-    # Retrievers help narrowing down the scope for the Reader to smaller units of text where a given question
+    # Retrievers help narrowing down the scope for the Reader to smaller units
+    # of text where a given question
     # could be answered.
-    #
-    # They use some simple but fast algorithm.
-    # **Here:** We use Elasticsearch's default BM25 algorithm
-
+    # We use Elasticsearch's default BM25 algorithm
     retriever = ElasticsearchRetriever(document_store=document_store)
-
     # ### Reader
-    #
-    # A Reader scans the texts returned by retrievers in detail and extracts the k best answers. They are based
-    # on powerful, but slower deep learning models.
-
+    # A Reader scans the texts returned by retrievers in detail and extracts
+    # the k best answers. It is based on a powerful, but slower deep learning model.
     reader = TransformersReader(
         model="dbmdz/bert-base-german-uncased", tokenizer="dbmdz/bert-base-german-uncased", use_gpu=-1)
-
     # ### Finder
-    #
-    # The Finder sticks together reader and retriever in a pipeline to answer our actual questions.
-
+    # The Finder sticks together reader and retriever in a pipeline to answer
+    # our actual questions.
     finder = Finder(reader, retriever)
-
     # You can configure how many candidates the reader and retriever shall return
-    # top_k_retriever: number of documents, top_k_reader: number of answers to be retunred
+    # top_k_retriever: number of documents, top_k_reader: number of answers to be returned
     prediction = finder.get_answers(
-        question="fährt das auto wenn der stecker steckt?", top_k_retriever=5, top_k_reader=3)
+        question="fährt das auto wenn der stecker steckt?",
+        top_k_retriever=5, top_k_reader=3)
     print_answers(prediction, details="all")
--- kbQA/kbQA_german_embeddings json_embeddings.py
+++ kbQA/kbQA_german_embeddings json_embeddings.py
@@ -24,29 +24,32 @@ def preprocessQuestion(text) -> str:
 if __name__ == '__main__':
     # Start new server or connect to a running one. true and false respectively
     LAUNCH_ELASTICSEARCH = False
-    # Determines whether the Elasticsearch Server has to be populated with data or not
+    # Determines whether the Elasticsearch Server has to be populated with data
+    # or not
     POPULATE_DOCUMENT_STORE = False
-
     # Start an Elasticsearch server
     if LAUNCH_ELASTICSEARCH:
         logging.info("Starting Elasticsearch ...")
         status = subprocess.run(
-            ['docker run -d -p 9200:9200 -e "discovery.type=single-node" elasticsearch:7.6.2'], shell=True
+            ['docker run -d -p 9200:9200 -e "discovery.type=single-node" elasticsearch:7.6.2'],
+            shell=True
         )
         if status.returncode:
-            raise Exception("Failed to launch Elasticsearch. If you want to connect to an existing Elasticsearch instance"
+            raise Exception("Failed to launch Elasticsearch. If you want to "
+                            "connect to an existing Elasticsearch instance"
                             "then set LAUNCH_ELASTICSEARCH in the script to False.")
         time.sleep(15)
 
     # Connect to Elasticsearch including embedding definition
-    document_store = ElasticsearchDocumentStore(host="localhost", username="", password="",
+    document_store = ElasticsearchDocumentStore(host="localhost",
+                                                username="",
+                                                password="",
                                                 index="document",
                                                 text_field="text",
                                                 embedding_field="question_emb",
                                                 embedding_dim="768",
                                                 excluded_meta_data=["question_emb"])
     # ## Initalize Retriever, Reader,  & Finder
-    #
     # ### Retriever
     # gpu= True to speed up processing
     # BERT-Model is trained to use the second extraction layer
@@ -55,7 +58,6 @@ if __name__ == '__main__':
         "\\kbQA\\bert-german-model",
         gpu=True, model_format="transformers",
         emb_extraction_layer=-2)
-
     if POPULATE_DOCUMENT_STORE:
         # set path to directory containing the embeddings
         doc_dir = os.getcwd() + "\\kbQA\\data\\tesla_embs.json"
@@ -66,14 +68,20 @@ if __name__ == '__main__':
         with open(doc_dir, encoding="utf8") as file:
             # initialize question indexing
             i = 0
-            # each line has multiple paragraphs and embeddings, read file line by line
+            # each line has multiple paragraphs and embeddings, read file line
+            # by line
             for cnt, line in enumerate(file):
                 # load the json string of the current line as a apython object
                 data = json.loads(line)
-                # add an entry in the dataframe for each paragraph in the current line
+                # add an entry in the dataframe for each paragraph in the current
+                # line
                 for j in range(len(data["paragraph"])):
                     df = df.append(
-                        {"question_emb": data["embeddings"][j], "name": data["file"], "text": data["paragraph"][j], "question": i}, ignore_index=True)
+                        {"question_emb": data["embeddings"][j],
+                            "name": data["file"],
+                            "text": data["paragraph"][j],
+                            "question": i},
+                        ignore_index=True)
                     i = i + 1
         # convert the dataframe to a dict
         docs_to_index = df.to_dict(orient="records")
@@ -103,7 +111,8 @@ if __name__ == '__main__':
     for question in questions:
         start_time = time.time()
         q = preprocessQuestion(question)
-        # get_answers only works with a reader attached, instead get the answer by comparing the embeddings
+        # get_answers only works with a reader attached, instead get the answer
+        # by comparing the embeddings
         prediction = finder.get_answers_via_similar_questions(
             question, top_k_retriever=5)
         times.append(time.time() - start_time)
